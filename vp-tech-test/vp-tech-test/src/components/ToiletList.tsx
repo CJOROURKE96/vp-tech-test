781a3fcd98ea4d493191b77f0d2711bec9511357
Connor  O'Rourke <connororourke@Connors-MacBook-Pro.local>
2024-10-27 15:06:26 +0000

    add PriceRange to the ToiletList

--- vp-tech-test/vp-tech-test/src/components/ToiletList.tsx
+++ vp-tech-test/vp-tech-test/src/components/ToiletList.tsx
@@ -3,10 +3,15 @@ import { fetchToilets, ToiletItem } from '../services/api';
 import StarRating from './StarRating';
 import '../styles/toiletList.css';
 import PriceDisplay from './PriceDisplay';
+import PriceFilter from './PriceFilter';
 
 const ToiletList: React.FC = () => {
   const [toilets, setToilets] = useState<ToiletItem[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [selectedRange, setSelectedRange] = useState<{
+    min: number;
+    max: number;
+  } | null>(null);
 
   useEffect(() => {
     const getToilets = async () => {
@@ -21,58 +26,58 @@ const ToiletList: React.FC = () => {
     getToilets();
   }, []);
 
+  const filteredToilets = toilets.filter((toilet) => {
+    if (!selectedRange) return true;
+    const price = toilet.price.priceIncTax;
+    return price >= selectedRange.min && price <= selectedRange.max;
+  });
+
   if (error) return <p>{error}</p>;
 
   return (
     <div>
-      <h1>Toilets</h1>
-      <ul
-        style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'flex-start',
-          margin: '0 -10px',
-          listStyleType: 'none',
-        }}
-      >
-        {toilets.map((toilet) => (
-          <li
-            key={toilet.id}
-            style={{
-              margin: '10px',
-              border: '1px solid #ddd',
-              padding: '10px',
-              width: 'calc(33.333% - 20px)',
-              boxSizing: 'border-box',
-            }}
-          >
+      <h1>Filter By</h1>
+      <div style={{ display: 'flex' }}>
+        <PriceFilter onFilterChange={setSelectedRange} toilets={toilets} />
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
+          {filteredToilets.map((toilet) => (
             <div
+              key={toilet.id}
               style={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '4px',
+                border: '1px solid #ddd',
+                padding: '10px',
+                width: 'calc(33.333% - 20px)',
+                boxSizing: 'border-box',
               }}
             >
-              <img
-                src={toilet.image.url}
-                alt={toilet.productName}
-                style={{ objectFit: 'cover' }}
-              />
-              <img
-                src={toilet.brand.brandImage.url}
-                alt={toilet.brand.name}
-                style={{ width: '100px', height: 'auto' }}
-              />
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: '4px',
+                }}
+              >
+                <img
+                  src={toilet.image.url}
+                  alt={toilet.productName}
+                  style={{ objectFit: 'cover' }}
+                />
+                <img
+                  src={toilet.brand.brandImage.url}
+                  alt={toilet.brand.name}
+                  style={{ width: '100px', height: 'auto' }}
+                />
+              </div>
+              <h2>{toilet.productName}</h2>
+              <PriceDisplay price={toilet.price} />
+              <div className="inline-reviews">
+                <StarRating rating={toilet.averageRating} />{' '}
+                <p>{toilet.reviewsCount}</p>
+              </div>
             </div>
-            <h2>{toilet.productName}</h2>
-            <PriceDisplay price={toilet.price} />
-            <div className="inline-reviews">
-              <StarRating rating={toilet.averageRating} />{' '}
-              <p>{toilet.reviewsCount}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
